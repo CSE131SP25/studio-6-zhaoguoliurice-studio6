15ba5bc1b183cb4ba0ae4cd83629130b0b3bdce1
alex_rice <alex_rice@localhost>
2025-03-05 14:16:22 -0600

    :(

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,12 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		  if (n == 0) {
+			  return 0;
+		  }
+		  else {
+		return (1.0 / (Math.pow(2, n))) + geometricSum(n - 1);
+		  }
 		
 	}
 
@@ -31,6 +36,10 @@ public class RecursiveMethods {
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
 		
 		// FIXME complete the recursive drawing
+		double bound = 5.0;
+		StdDraw.setXscale(-bound, bound);
+		StdDraw.setYscale(-bound, bound);
+		
 	}
 	
 
